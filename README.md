a254007334871209f87f30fb3439e69d56e66009
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2022-09-04 17:33:39 +0300

    Project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Projects List
+## 1. Hello world
+Renders text with rainbow color change and rotation.
+![Hello world preview](hello-world/preview.png 'Hello world')
---
+++ __empty-example/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    // put setup code here
+    textSize(width / 5);
+    textAlign(CENTER, CENTER);
+    colorMode(HSL);
+    background(0);
+}
+
+function draw() {
+    // put drawing code here
+    background(0, 0.1);
+
+    let time = millis();
+    translate(width / 2, height / 2);
+    rotate(time / 1000);
+
+    fill(frameCount % 360, 100, 50);
+    text('hello world', 0, 0);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    textSize(width / 5);
+}
